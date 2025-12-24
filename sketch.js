ec565551498bd0f56da1f7530f7ad65c3c95acb2
begumgokmen <bg2838@barnard.edu>
2025-12-24 14:07:28 -0500

    fix

--- sketch.js
+++ sketch.js
@@ -1,110 +1,73 @@
-let angle = 0;
-let lastMinute = -1;
-let cubeColors = [];
-
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(600, 600);
   angleMode(DEGREES);
-  initCubeColors();
+  noStroke();
+  frameRate(1); // Update every second
 }
 
 function draw() {
-  background(10);
-  lights();
+  background(20);
+  translate(width / 2, height / 2);
 
-  rotateX(-20);
-  rotateY(angle);
-
-  let cubeSize = 180;
-  let small = cubeSize / 3;
-
-  for (let x = -1; x <= 1; x++) {
-    for (let y = -1; y <= 1; y++) {
-      for (let z = -1; z <= 1; z++) {
-        push();
-        translate(x * small, y * small, z * small);
-        drawCubelet(cubeColors[x + 1][y + 1][z + 1], small * 0.95);
-        pop();
-      }
-    }
-  }
-
-  // Rotate every minute
+  // Time values
+  let h = hour() % 24;
   let m = minute();
-  if (m !== lastMinute) {
-    angle += 90;
-    lastMinute = m;
-    console.log("Minute:", m);
-    updateCubeColors();
-  }
-}
+  let s = second();
 
-function initCubeColors() {
-  for (let x = 0; x < 3; x++) {
-    cubeColors[x] = [];
-    for (let y = 0; y < 3; y++) {
-      cubeColors[x][y] = [];
-      for (let z = 0; z < 3; z++) {
-        cubeColors[x][y][z] = {
-          x: color(255, 0, 0),     // red (right)
-          y: color(0, 255, 0),     // green (top)
-          z: color(255, 255, 255), // white (front)
-        };
-      }
+  let tileSize = 40;
+  let xOffset = -2 * tileSize;
+  let yOffset = -3 * tileSize;
+
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      // Calculate color based on time and tile index
+      let hueFront = map(h + row + col, 0, 24, 0, 360);
+      let hueTop = map(m + row + col, 0, 60, 0, 360);
+      let hueRight = map(s + row + col, 0, 60, 0, 360);
+
+      drawIsometricSticker(
+        xOffset + col * tileSize,
+        yOffset + row * tileSize,
+        tileSize,
+        color(hueFront, 100, 100), // front
+        color(hueTop, 100, 100),   // top
+        color(hueRight, 100, 100)  // side
+      );
     }
   }
 }
 
-function updateCubeColors() {
-  // Gradually solve the cube over 24 hours
-  let h = hour();
-  let progress = map(h % 24, 0, 24, 0, 255);
-
-  for (let x = 0; x < 3; x++) {
-    for (let y = 0; y < 3; y++) {
-      for (let z = 0; z < 3; z++) {
-        cubeColors[x][y][z] = {
-          x: color(progress, 0, 255 - progress), // shift red to blue
-          y: color(0, progress, 255 - progress), // shift green
-          z: color(255 - progress),              // fade to white
-        };
-      }
-    }
-  }
-}
-
-function drawCubelet(c, size) {
-  let s = size / 2;
-
-  // Outline cube
+function drawIsometricSticker(x, y, size, frontColor, topColor, sideColor) {
   push();
-  stroke(30);
-  noFill();
-  box(size);
-  pop();
+  colorMode(HSL, 360, 100, 100);
+  noStroke();
 
-  // FRONT FACE (Z+)
-  push();
-  translate(0, 0, s + 0.5);
-  fill(c.z); noStroke();
-  plane(size - 4, size - 4);
-  pop();
+  // FRONT
+  fill(frontColor);
+  beginShape();
+  vertex(x, y);
+  vertex(x + size, y);
+  vertex(x + size - 20, y + 20);
+  vertex(x - 20, y + 20);
+  endShape(CLOSE);
 
-  // TOP FACE (Y-)
-  push();
-  translate(0, -s - 0.5, 0);
-  rotateX(HALF_PI);
-  fill(c.y); noStroke();
-  plane(size - 4, size - 4);
-  pop();
+  // TOP
+  fill(topColor);
+  beginShape();
+  vertex(x, y);
+  vertex(x - 20, y + 20);
+  vertex(x - 20, y + 20 - size);
+  vertex(x, y - size);
+  endShape(CLOSE);
 
-  // RIGHT FACE (X+)
-  push();
-  translate(s + 0.5, 0, 0);
-  rotateY(HALF_PI);
-  fill(c.x); noStroke();
-  plane(size - 4, size - 4);
+  // SIDE
+  fill(sideColor);
+  beginShape();
+  vertex(x + size, y);
+  vertex(x + size - 20, y + 20);
+  vertex(x + size - 20, y + 20 - size);
+  vertex(x + size, y - size);
+  endShape(CLOSE);
+
   pop();
 }
-
-
